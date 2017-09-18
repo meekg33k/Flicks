1d3287cf346b283c23a69432af3edd548a518e7c
Uduak 'Eren <sandy3jnr@gmail.com>
2017-09-17 18:47:43 -0700

    Implemented activity to allow user view details of movie

--- app/src/main/java/me/uduakobongeren/flicks/activities/MovieDetailsActivity.java
+++ app/src/main/java/me/uduakobongeren/flicks/activities/MovieDetailsActivity.java
@@ -2,6 +2,10 @@ package me.uduakobongeren.flicks.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import me.uduakobongeren.flicks.R;
 
@@ -16,5 +20,28 @@ public class MovieDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
+
+        Bundle extras = getIntent().getExtras();
+
+        if (extras != null){
+            String movieTitle = extras.getString("title");
+            String movieOverview = extras.getString("overview");
+            String imageURL = extras.getString("image_url");
+            String date = extras.getString("date");
+
+            TextView tvTitle = (TextView) findViewById(R.id.tvTitle);
+            tvTitle.setText(movieTitle);
+
+            TextView tvOverview = (TextView) findViewById(R.id.tvOverview);
+            tvOverview.setText(movieOverview);
+
+            TextView tvReleaseDate = (TextView) findViewById(R.id.tvDate);
+            tvReleaseDate.setText(date);
+
+            ImageView ivPosterImage = (ImageView) findViewById(R.id.ivPoster);
+            Picasso.with(this).load(imageURL).into(ivPosterImage);
+
+        }
+
     }
 }
